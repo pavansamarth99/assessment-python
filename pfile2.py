0c91bbe957284ddb91ee5590f9d9945819c542f3
pavansamarth99 <bgpavanvittal@kpmg.com>
2021-09-08 19:58:53 +0530

    added second python file

---
+++ pfile2.py
@@ -0,0 +1,14 @@
+test_list = [[('Gfg', 3), ('is', 3)], [('best', 1)], [('for', 5), ('geeks', 1)]]
+  
+# printing original list
+print("The original list is : " + str(test_list))
+  
+# initializing Custom eles
+cus_eles = [6, 7, 8]
+  
+# Row-wise element Addition in Tuple Matrix
+# Using enumerate() + list comprehension
+res = [[sub + (cus_eles[idx], ) for sub in val] for idx, val in enumerate(test_list)]
+  
+# printing result 
+print("The matrix after row elements addition : " + str(res)) 
